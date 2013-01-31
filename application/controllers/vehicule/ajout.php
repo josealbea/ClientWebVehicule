67860a6eaeabb36f8e2b0c56a9a9d237aa0a62bb
teisseire <yoyot92@hotmail.com>
2013-01-31 19:46:19 +0100

    modif

--- application/controllers/vehicule/ajout.php
+++ application/controllers/vehicule/ajout.php
@@ -1,17 +1,26 @@
 <?php 
 
-function ajoutvehicule() {
-	$service_url = "http://achetervehicule.com/webservice/?controller=vehicule&action=index";
-	$curl = curl_init($service_url);
-	$curl_post_data = array(
-	    "user_id" => 42,
-	    "emailaddress" => 'lorna@example.com',
-	    );
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($curl, CURLOPT_POST, true);
-	curl_setopt($curl, CURLOPT_POSTFIELDS, $curl_post_data);
-	$curl_response = curl_exec($curl);
-	curl_close($curl);
+ajoutAnnonce();
 
-	$xml = new SimpleXMLElement($curl_response);
+// modifier cette ^page ::
+
+function ajoutAnnonce() {
+	$curl = curl_init($service_url);
+	if (!empty($_POST)) {
+		$formData = $_POST;
+		$vars="password=".$formData['password']."&mail=".$formData['mail']."&nom=".$formData['nom']."&ville=".$formData['ville']."&code_postal=".$formData['cp'];
+		// echo $vars;
+		$ch=curl_init('http://localhost/projetB3/users/index');
+		curl_setopt($ch,CURLOPT_POST, true);
+		curl_setopt($ch,CURLOPT_POSTFIELDS,$vars);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$ret = curl_exec($ch);
+		if (!$ret) {
+		    echo curl_error($ch);
+		} else {
+		    return $erreur = "L'adresse mail existe déjà";
+		}
+
+		curl_close($ch);
+	}
 }
