7922cacf56f89f91ce6e29841f6b6c11f7fe61fd
teisseire <yoyot92@hotmail.com>
2013-02-04 10:07:36 +0100

    déposez une annonce

--- application/controllers/vehicule/ajout.php
+++ application/controllers/vehicule/ajout.php
@@ -1,26 +1,29 @@
-<?php 
+ <?php 
 
 ajoutAnnonce();
 
-// modifier cette ^page ::
+// modifier cette page ::
 
 function ajoutAnnonce() {
 	$curl = curl_init($service_url);
 	if (!empty($_POST)) {
 		$formData = $_POST;
-		$vars="password=".$formData['password']."&mail=".$formData['mail']."&nom=".$formData['nom']."&ville=".$formData['ville']."&code_postal=".$formData['cp'];
-		// echo $vars;
-		$ch=curl_init('http://localhost/projetB3/users/index');
+		if($formData['categorie'] == 1){
+			$vehicule = "&boite_vitesse=".$formData['boite_vitesse']."&nb_places=".$formData['nb_places'];
+		}
+		else{
+			$vehicule = "cylindree=".$formData['cylindree'];
+		}
+		$vars="titre=".$formData['titre']."&description=".$formData['description']."&prix=".$formData['prix']."&annee=".$formData['annee']."&km=".$formData['km']."&energie=".$formData['energie']."&id_categorie=".$formData['categorie'].$vehicule;
+		//echo $vars;
+		$ch=curl_init('http://localhost/projetB3/vehicule/index');
 		curl_setopt($ch,CURLOPT_POST, true);
 		curl_setopt($ch,CURLOPT_POSTFIELDS,$vars);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		$ret = curl_exec($ch);
 		if (!$ret) {
 		    echo curl_error($ch);
-		} else {
-		    return $erreur = "L'adresse mail existe déjà";
 		}
-
 		curl_close($ch);
 	}
 }
