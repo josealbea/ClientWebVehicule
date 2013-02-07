328651d21edb34f4c522daad4f2540b5ef4e39ca
josealbea <jose.albea@gmail.com>
2013-02-07 09:19:06 +0100

    Modif upload

--- application/controllers/vehicule/single.php
+++ application/controllers/vehicule/single.php
@@ -10,6 +10,6 @@ function loadXMLFile() {
   global $vehicule;
   $dom = new DOMDocument();
   $dom->preserveWhiteSpace = false;
-  $dom->Load("http://www.josealbea.com/projetB3/webservice/?controller=vehicule&action=single&id=".$_GET['id']);
+  $dom->Load("http://localhost/projetB3/?controller=vehicule&action=single&id=".$_GET['id']);
   $vehicule = $dom->getElementsByTagName("vehicule");
 }
