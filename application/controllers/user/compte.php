302c8f017c8981c8db053692494e5c439ac83af4
teisseire <yoyot92@hotmail.com>
2013-02-09 16:58:15 +0100

    page mon compte

--- application/controllers/user/compte.php
+++ application/controllers/user/compte.php
@@ -1,6 +1,11 @@
 <?php
 
 monCompte("http://localhost/projetB3/?controller=users&action=single&id=".$_SESSION['id_membre']);
+listerVehicule("http://localhost/projetB3/?controller=vehicule&action=index&id_membre=".$_SESSION['id_membre']);
+
+if (isset($_GET['delete'])) {
+	deleteVehicule($_GET['id']);
+}
 
 function monCompte($url) 
 {
@@ -34,4 +39,29 @@ function modifCompte() {
 		}
 		curl_close($ch);
 	}
+
 }
+	function listerVehicule($url){
+	global $liste;
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	$result = utf8_decode(curl_exec($ch));
+	curl_close($ch);
+	$listes = new DOMDocument();
+	$listes->loadXML($result);
+	$liste=$listes->getElementsByTagName("vehicule");
+
+	return $liste;
+}
+ function deleteVehicule($id_vehicule){
+ 	$request_body = $id_vehicule;
+	$ch = curl_init('http://localhost/projetB3/?controller=vehicule&action=single&id='.$id_vehicule);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $request_body);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+    $response = curl_exec($ch);
+    header('location: http://localhost/ClientWebVehicule/user/compte');
+ }
