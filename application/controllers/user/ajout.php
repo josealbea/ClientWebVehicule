0baef166c846ea5cc4855d3201a572341aceba9e
josealbea <jose.albea@gmail.com>
2013-02-12 20:36:40 +0100

    j

--- application/controllers/user/ajout.php
+++ application/controllers/user/ajout.php
@@ -7,20 +7,22 @@ function init() {
 }
 
 function ajoutMembre() {
+	global $error;
 	$curl = curl_init($service_url);
 	if (!empty($_POST)) {
 		$formData = $_POST;
 		$vars="password=".$formData['password_inscript3']."&mail=".$formData['mail_inscript3']."&nom=".$formData['nom3']."&code_postal=".$formData['cp3']."&telephone=".$formData['tel3'];
-		echo $vars;
+		//echo $vars;
 		$ch=curl_init(API_ROOT.'users/index');
 		curl_setopt($ch,CURLOPT_POST, true);
 		curl_setopt($ch,CURLOPT_POSTFIELDS,$vars);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		$ret = curl_exec($ch);
+		echo $ret;exit;
 		if (!$ret) {
 		    echo curl_error($ch);
 		} else {
-		    echo "L'adresse mail existe déjà";
+		    $error = "L'adresse mail existe déjà";
 		}
 
 		curl_close($ch);
