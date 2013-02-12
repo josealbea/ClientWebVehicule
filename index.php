d8d1b693a2edbb3000c0abcf5b4e80c5dbeaad48
josealbea <jose.albea@gmail.com>
2013-02-12 10:14:23 +0100

    j

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php 
 session_start();
-$_SESSION['id_membre'] = 1;
 define("APPLICATION_PATH", dirname(__FILE__)."/application/");
 define("SITE_ROOT", "http://localhost/ClientWebVehicule/");
 define("API_ROOT", "http://localhost/projetB3/");
